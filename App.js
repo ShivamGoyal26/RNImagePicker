8118e24f1201d48618f598a4a3680f4327078aea
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-02-05 11:42:07 +0530

    Picking Images from Native Storage

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = props => {
+  return(
+    <View style={styles.screen}>
+      <Text>This is the main Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default App;
